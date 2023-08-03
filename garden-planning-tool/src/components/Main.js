78c955095c5835c25f60a5bb774d79f5aa73c7c9
Carolista <cjones.renaissancewoman@gmail.com>
2023-08-02 21:23:52 -0500

    added Main component

---
+++ garden-planning-tool/src/components/Main.js
@@ -0,0 +1,13 @@
+const Main = () => {
+
+  return (
+    <main>
+      <div id="left-column">
+        <h3>Available Plants</h3>
+        <p>plants will go here...</p>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
