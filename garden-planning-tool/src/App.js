4bf64172cbc5f96bac044f4d83cd1602ce2c9e67
Carolista <cjones.renaissancewoman@gmail.com>
2023-08-02 21:16:17 -0500

    created Header component

--- garden-planning-tool/src/App.js
+++ garden-planning-tool/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      Place components here!
-    </div>
+    <Header />
   );
 }
 
---
+++ garden-planning-tool/src/components/Header.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+// Example of class-based component (phasing out)
+export default class Header extends Component {
+  render () {
+    return (
+      <header>
+        <h2>Garden Planning Tool</h2>
+      </header>
+    );
+  }
+}
