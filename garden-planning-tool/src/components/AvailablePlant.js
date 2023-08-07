fb5019a717e6b9f18b541d33a6c9f2f187078c25
Carolista <cjones.renaissancewoman@gmail.com>
2023-08-06 22:42:53 -0500

    updated AvailablePlant component to display a full card with clickable icons

--- garden-planning-tool/src/components/AvailablePlant.js
+++ garden-planning-tool/src/components/AvailablePlant.js
@@ -1,11 +1,49 @@
+import styles from "./AvailablePlant.module.css";
+import { capitalize } from "../shared/utils";
+
 const AvailablePlant = props => {
-  
-  // TODO #2: Upgrade to display a card with image, chevron icons, etc. 
-  // TODO #3: Add event handlers to icons (just log something for now)
 
-  let { id, name, color, numAvailable } = props.plant;
+  let { id, name, color, image, numAvailable } = props.plant;
 
-  return <p id={id}>{name} - {color} - {numAvailable} available</p>;
+  const handleReturnPlant = () => {
+    console.log(`Returning 1 ${color} ${name}.`)
+
+  };
+
+  const handleAllocatePlant = () => {
+    console.log(`Allocating 1 ${color} ${name}.`)
+  };
+
+  return (
+    <div id={id} className={`${styles.card} ${styles["available-plant"]}`}>
+      <div className={styles["image-and-text"]}>
+        <img
+          src={image}
+          width="50px"
+          height="50px"
+          alt={`${color} ${name}`}
+        />
+        <p>
+          {name}, {capitalize(color)}<br /> 
+          <span className={styles["amount-text"]}>
+            {numAvailable} available
+          </span>
+        </p>
+      </div>
+      <div className={styles.arrows}>
+        <span
+          className={`fa fa-chevron-circle-left ${styles["chevron-left"]}`}
+          title="Remove one plant from flowerbed"
+          onClick={handleReturnPlant}
+        ></span>
+        <span
+          className={`fa fa-chevron-circle-right ${styles["chevron-right"]}`}
+          title="Add one plant to flowerbed"
+          onClick={handleAllocatePlant}
+        ></span>
+      </div>
+    </div>
+  );
 }
 
 export default AvailablePlant;
--- garden-planning-tool/src/components/Main.js
+++ garden-planning-tool/src/components/Main.js
@@ -8,21 +8,21 @@ const Main = () => {
       id: 1,
       name: "Hyacinth",
       color: "blue",
-      image: "hyacinth-blue.png",
+      image: "https://drive.google.com/uc?export=view&id=1wZU8kWKcTWCIEMjz-Ce7dTU8Fcb-NM9I",
       numAvailable: 6
     },
     {
       id: 2,
       name: "Tulip",
       color: "red",
-      image: "tulip-red.png",
+      image: "https://drive.google.com/uc?export=view&id=1Vl0Ch4MQslwh9EyhnGX1RK9YZbPuHtvo",
       numAvailable: 6
     },
     {
       id: 3,
       name: "Daffodil",
       color: "yellow",
-      image: "daffodil-yellow.png",
+      image: "https://drive.google.com/uc?export=view&id=1BwQ_BdSgB6XGdU_OOm5iYQFXKjKEcW1Q",
       numAvailable: 4
     },  
   ];
@@ -32,30 +32,29 @@ const Main = () => {
       id: 1,
       name: "Hyacinth",
       color: "blue",
-      image: "hyacinth-blue.png",
+      image: "https://drive.google.com/uc?export=view&id=1wZU8kWKcTWCIEMjz-Ce7dTU8Fcb-NM9I",
       numAvailable: 2
     },
     {
       id: 2,
       name: "Tulip",
       color: "red",
-      image: "tulip-red.png",
+      image: "https://drive.google.com/uc?export=view&id=1Vl0Ch4MQslwh9EyhnGX1RK9YZbPuHtvo",
       numAvailable: 6
     },
     {
       id: 3,
       name: "Daffodil",
       color: "yellow",
-      image: "daffodil-yellow.png",
+      image: "https://drive.google.com/uc?export=view&id=1BwQ_BdSgB6XGdU_OOm5iYQFXKjKEcW1Q",
       numAvailable: 12
     },  
   ];
 
   let availablePlants = testPlants1.map(plant => {
-    return <AvailablePlant plant={plant} />;
+    return <AvailablePlant key={plant.id} plant={plant} />;
   });
 
-  // TODOs #2 and #3: Go to AvailablePlant component and upgrade content
   // TODO #4: Add right column below with Flowerbed component that has a selectedPlants prop — test passing an empty array and then testPlants2
 
   return (
