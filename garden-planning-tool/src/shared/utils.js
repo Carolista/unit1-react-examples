0652756b3517aaf38202fc7cae38044cb394d5fc
Carolista <cjones.renaissancewoman@gmail.com>
2023-08-07 01:37:53 -0500

    added utils with helper functions

---
+++ garden-planning-tool/src/shared/utils.js
@@ -0,0 +1,7 @@
+export function alphabetize(arr, key) {
+  return arr.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+}
+
+export function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
